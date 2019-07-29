9304f68ba4a1e404a855ab10be4bab3a5bd3e335
mykindofscum <alvinkchan99@gmail.com>
2019-07-28 17:21:52 -0700

    Homepage refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import inlogo from './In-logo.png';
+import signinLogo from './signinbtn@3x.png';
+import joinNow from './Joinnow@3x.png';
 import peerlogo from './Linkedin_Peer_Logo-03.png';
 import { Link } from 'react-router-dom';
 // import LoginPage from '../../components/LoginPage/LoginPage';
@@ -27,22 +28,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="heading"><h3>Explore. Connect. Inspire</h3></div>
         <section className="peerLogo" id="html">
-          <img src={peerlogo} height={58} width={118} alt="logo" /><br></br>
-          {/* <img src={signinbutton} height={60} width={200}/><br></br> */}
+          <img src={peerlogo} height={50} width={118} alt="logo" /><br></br>
         </section>
-        <div>
-
+        <div className="LoginButton">
+          <img src={signinLogo} height={50} width={200} />
         </div>
-          {/* <ButtonGroup vertical>
-            <Link to="/login"><Button variant="primary" size="lg" className="SignInButton">
-              Sign In with LinkedIn
-            </Button></Link>
-
-            <Button variant="secondary" size="lg" className="JoinButton">
-              Join now
-            </Button>
-          </ButtonGroup> */}
+        <div className="JoinNow">
+          <img src={joinNow} height={43} width={130} />
+        </div>
       </div>
       );
     }
